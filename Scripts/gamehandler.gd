1ddc0e2df624ef8638d7cb5547296b8e31f12425
ikicapitan <ikicapitan141@gmail.com>
2018-09-15 02:06:57 -0300

    Some Menues
    - Panel Para Futuros Menus.
    - Texto de Fases.
    - Ejemplo de Recursos y Panel.

--- Scripts/gamehandler.gd
+++ Scripts/gamehandler.gd
@@ -2,8 +2,13 @@ extends Node #Clase maestra del juego (madrE)
 
 enum estados {fase_1, fase_2, fase_3} #Fase 1: Preparacion #Fase 2: Batalla #Fase 3: Puntajes
 var e_actual = fase_1 #Fase actual del juego
+var t_fase1 = 60 #Preparacion dura 60 segundos
+var t_fase2 = 60*3 #Batalla dura 3 minutos
+var t_fase3 = 10 #Puntajes dura 10 segundos
+var tiempo = 60 #Tiempo restante de la fase actual
 
-var tiempo = 60 #Tiempo restante de la fase
+var oro = 100
+var alimento = 100
 
 func clock(): #Cuando pasa un segundo se llamara a esta funcion
 	tiempo -= 1
@@ -11,10 +16,18 @@ func clock(): #Cuando pasa un segundo se llamara a esta funcion
 		time_up()
 	
 func time_up(): #Se termino la fase
+	var txt_fase = "Fase: "
 	match(e_actual): #Pasar a fase siguiente
 		fase_1:
 			fase_2
+			txt_fase += "BATALLA"
+			tiempo = t_fase2
 		fase_2:
 			fase_3
+			txt_fase += "PUNTAJES"
+			tiempo = t_fase3
 		fase_3:
 			fase_1
+			txt_fase += "PREPARACION"
+			tiempo = t_fase1
+	get_tree().get_nodes_in_group("fase")[0].text = txt_fase #Asignamos texto de fase actual
